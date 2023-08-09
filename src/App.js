6ea55d3bd967860e64a1b0b11180ddd236a3093a
sallalhaider <sallalhaider8@gmail.com>
2023-08-09 15:11:02 +0500

    third commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import Home from './Components/Home';
-import { Login } from './Components/Login';
+import Login  from './Components/Login';
+import Movies from './Components/Movies';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-     <Login />
+     <Movies />
     
   
   
---
+++ src/Utilities/api.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+
+const BASE_TOKEN = "eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJlMzNkMGVmOGVmMGFjMzMwNmIwYzQ3NTExMzcyMzQ1NSIsInN1YiI6IjY0OTBhZWFlYzJmZjNkMDBmZmJjZGUxOSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.BIuWnK_1G62T5MHLVGyiGpDY1qwX1K_E8yu8Ijh36oM";
+const BASE_URL = "https://www.themoviedb.org/3/";
+
+const headers = {
+    Authorization: "bearer " + BASE_TOKEN,
+};
+
+export const fetchDataFromApi = async (url, params) => {
+    try {
+        const data  = await axios.get(BASE_URL + url, {
+            headers,
+            params,
+        });
+        return data;
+    } catch (err) {
+        console.log(err);
+        return err;
+    }
+};
